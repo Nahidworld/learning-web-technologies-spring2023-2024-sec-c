5594ff12ae51fc9cedb70b0b1e1c09c2c5d25b0b
Nahidworld <157556719+Nahidworld@users.noreply.github.com>
2024-02-18 16:49:41 +0600

    Add files via upload
    Signed-off-by: Nahidworld <157556719+Nahidworld@users.noreply.github.com>

---
+++ Lab 5/1login.php
@@ -0,0 +1,12 @@
+<?php
+
+    $username = $_REQUEST["username"];
+    $password = $_REQUEST["pass"];
+
+    if(strlen($name)<=8 || $name >= a && $name <= z){
+        echo "Error in username";
+    }
+
+    
+    
+?>
