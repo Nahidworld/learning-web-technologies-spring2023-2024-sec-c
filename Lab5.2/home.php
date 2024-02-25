d510d90d46eadc05b796f872c0a9866186227640
Nahidworld <157556719+Nahidworld@users.noreply.github.com>
2024-02-25 16:28:01 +0600

    Add files via upload
    Signed-off-by: Nahidworld <157556719+Nahidworld@users.noreply.github.com>

---
+++ Lab5.2/home.php
@@ -0,0 +1,23 @@
+<?php 
+    //session_start();
+
+    //if(!isset($_SESSION['flag'])){
+    //    header('location: login.php');
+    //}
+?>
+
+
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <img src="">
+    <a href="home.php">Home</a>|
+    <a href="login.php">Login</a>|
+    <a href="registration.php">Registration</a>
+    <?php //echo $_SESSION['username'];?>
+    <h1>Welcome to xCompany </h1>
+    <center><p>Copyright @2017 </p></center>
+</body>
+</html>
