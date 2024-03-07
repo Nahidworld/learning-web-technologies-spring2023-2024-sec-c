c8d3bb4a3497b010b95ed717198520fe66b7bcea
NahidWorld <fahimbinimam@gmail.com>
2024-03-07 12:08:45 +0600

    Updated php pattern

--- LAB-2 [PHP]/task7.php
+++ LAB-2 [PHP]/task7.php
@@ -1,13 +1,59 @@
 <?php
     //Print the following shapes
     // Hints: use NESTED LOOP
+   
+    
+    
+    
+    // For Star pattern:
     // *
     // * *
-    // * * *1 2 3
+    // * * *
+    for ($i =1; $i <= 3; $i++)
+    {
+        for ($j = 1; $j <= $i; $j++){
+            echo "x";
+            if ($j < $i){
+                echo " ";
+            }
+        }
+        print "<br>";
+    }
+
+    //For number Pattern:
+    // 1 2 3
     // 12
     // 1
-    // A
-    // B C
-    // D E F
+    for ($i=3; $i<=1; $i--){
+        for ($j=1; $j<=$i ;$j++){
+            echo $j;
+            if ($j <$i){
+                echo " ";
+            }
+        }
+        print "<br>";
+    }
+
+    //For Alphabetic triangle:
+    $char ='A';
+    //$count = 1;
+    for ($i =1; $i<=3; $i++){
+        $count =1;
+        for ($j=1; $j <= $i; $j++){
+            echo $char++;
+            if($count<3){
+                echo " ";
+                $count++;
+            }
+        }
+        echo "<br>";
+    }
+
+
+    
+    
+
+
+
 
 ?>
