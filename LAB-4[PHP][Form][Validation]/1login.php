683e6e1e2b0f2ddaa3d001902378c60ef549fa89
NahidWorld <fahimbinimam@gmail.com>
2024-03-18 19:14:32 +0600

    updated login and password change

--- LAB-4[PHP][Form][Validation]/1login.php
+++ LAB-4[PHP][Form][Validation]/1login.php
@@ -1,12 +1,56 @@
-<?php
+<html>
+<head>
+    <title>Login</title>
+</head>
 
-    $username = $_REQUEST["username"];
-    $password = $_REQUEST["pass"];
+<body>
+    <form  action="1login.php" method="post">
+        <fieldset style="width: 30%;">
+            <legend><b>LOGIN</b></legend>
 
-    if(strlen($name)<=8 || $name >= a && $name <= z){
-        echo "Error in username";
+            <table border="0">
+                <tr>
+                    <td>UserName</td>
+                    <td>: <input type="text" value="" name="username" title=""></td>
+                </tr>
+                <tr>
+                    <td>Password </td>
+                    <td>: <input type="password" value="" name="password" title=""></td>
+                </tr>
+            </table>
+
+            <hr>
+            <input type="checkbox" value="" name="">
+            <label for="remember">Remember Me</label>
+            <br><br>
+            <input type="submit" value="Submit" name="submit">
+            <a href="2changepassword.php">
+                <label for="forgot_password">Forgot Password?</label>
+            </a>
+        </fieldset>
+
+    </form>
+</body>
+</html>
+
+
+<?php 
+    if(isset($_POST['submit'])){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        
+
+        if(empty($username)){
+            echo "User Name is missing";
+        }elseif(empty($password)){
+            echo "Password is missing";
+        }elseif(strlen($username)<2){
+            echo "User Name must contain at least two (2) character";
+        }elseif(strlen($password)<8){
+            echo "Password must contain at least eight (8) character";
+        }
+    
+
     }
-
-    
-    
 ?>
+
---
+++ LAB-4[PHP][Form][Validation]/2changepassword.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+    <title>
+        Change Password
+    </title>
+</head>
+
+<body>
+    <form action="2changepassword.php" method="post" enctype="">
+        <fieldset style = "width: 30%">
+            <legend><b>Change Password</b></legend>
+            <table border="0">
+                <tr>
+                    <td>
+                        Current Password 
+                    </td>
+                    <td>
+                        : <input type="password" value="" name="current_password">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p style="color: #1b881b;">New Password</p>
+                    </td>
+                    <td>
+                        : <input type="password" value="" name="new_password">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p style="color: #9e0606;">Retype New Password</p>
+                    </td>
+                    <td>
+                        : <input type="password" value="" name="retype_new_password">
+                    </td>
+                </tr> 
+            </table>
+            <hr>
+            <input type="submit" value="Submit" name="submit">
+        </fieldset>
+    </form>
+</body>
+</html>
+
+<?php
+    if(isset($_POST['submit'])){
+        $current_pass = $_POST["current_password"];
+        $new_pass = $_POST["new_password"];
+        $retype_new_pass = $_POST["retype_new_password"];
+        
+        if($current_pass == $new_pass){
+            echo "New Password should not be same as the Current Password";
+        }elseif($new_pass != $retype_new_pass){
+            echo "New Password must match with the Retyped Password";
+        }else{
+            echo "Password Changed Successfully!";
+        }
+
+    }
+
+?>
