210e750edf21541f1e58463983e9d93d95b5c704
Nahid <157556719+Nahidworld@users.noreply.github.com>
2024-03-26 02:53:43 +0600

    Update addProduct.php
    Signed-off-by: Nahid <157556719+Nahidworld@users.noreply.github.com>

--- LAB-6 [PHP] [DB CRUD Operations]/addProduct.php
+++ LAB-6 [PHP] [DB CRUD Operations]/addProduct.php
@@ -1,28 +1,45 @@
 <?php
-    require_once('db.php');
+require_once('db.php');
 
-    function addProduct($name, $buying_price, $selling_price, $display){
-        $con = dbConnect();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $buying_price = $_POST['buying_price'];
+    $selling_price = $_POST['selling_price'];
+    $display = isset($_POST['display']) ? 1 : 0; // Check if display checkbox is checked
 
-        //$sql = "insert into users values('', '{$user['username']}','{$user['email']}', '{$user['password']}')";
+    $con = dbConnect();
+
+    $name = mysqli_real_escape_string($con, $name);
+    $buying_price = mysqli_real_escape_string($con, $buying_price);
+    $selling_price = mysqli_real_escape_string($con, $selling_price);
+
+    $sql = "INSERT INTO products (name, buying_price, selling_price, display) VALUES ('$name', '$buying_price', '$selling_price', '$display')";
+
+    if(mysqli_query($con, $sql)){
+        echo "Product added successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
     }
+    mysqli_close($con);
+}
 ?>
+
 <html>
-    <head>
-        <title>Add Product</title>
-    </head>
-    <body>
-        <form method="post" action="addProduct.php" enctype="">
-            <fieldset style="width: 50%;">
-                <legend>ADD PRODUCT</legend>
-                Name <br><input type="text" name="name" value=""> <br> 
-                Buying Price <br><input type="number" name="buying_price" value=""> <br>
-                Selling Price <br><input type="number" name="selling_price" value=""> <br>
-                <hr>
-                <input type="checkbox" name="display" value=""> Display 
-                <hr>
-                <input type="submit" name="save" value="Save">
-            </fieldset>
-        </form>
-    </body>
+<head>
+    <title>Add Product</title>
+</head>
+<body>
+<form method="post" action="addProduct.php" enctype="">
+    <fieldset style="width: 50%;">
+        <legend>ADD PRODUCT</legend>
+        Name <br><input type="text" name="name" value=""> <br>
+        Buying Price <br><input type="number" name="buying_price" value=""> <br>
+        Selling Price <br><input type="number" name="selling_price" value=""> <br>
+        <hr>
+        <input type="checkbox" name="display" value=""> Display
+        <hr>
+        <input type="submit" name="save" value="Save">
+    </fieldset>
+</form>
+</body>
 </html>
