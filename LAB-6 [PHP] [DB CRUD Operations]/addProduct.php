64c69e3974105155e021c36669bdb7fc1d6f8e07
Nahid <157556719+Nahidworld@users.noreply.github.com>
2024-03-26 03:05:27 +0600

    Update addProduct.php
    Signed-off-by: Nahid <157556719+Nahidworld@users.noreply.github.com>

--- LAB-6 [PHP] [DB CRUD Operations]/addProduct.php
+++ LAB-6 [PHP] [DB CRUD Operations]/addProduct.php
@@ -5,22 +5,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $buying_price = $_POST['buying_price'];
     $selling_price = $_POST['selling_price'];
-    $display = isset($_POST['display']) ? 1 : 0; // Check if display checkbox is checked
 
-    $con = dbConnect();
+    if (!empty($name) && !empty($buying_price) && !empty($selling_price)) {
+        $con = dbConnect();
+        
+        $name = mysqli_real_escape_string($con, $name);
+        $buying_price = mysqli_real_escape_string($con, $buying_price);
+        $selling_price = mysqli_real_escape_string($con, $selling_price);
 
-    $name = mysqli_real_escape_string($con, $name);
-    $buying_price = mysqli_real_escape_string($con, $buying_price);
-    $selling_price = mysqli_real_escape_string($con, $selling_price);
+        if(isset($_POST['display'])) {
+            $display = 1;
+        } else {
+            $display = 0;
+        }
+        
+        $sql = "INSERT INTO products (name, buying_price, selling_price, display) 
+        VALUES ('$name', '$buying_price', '$selling_price', '$display')";
 
-    $sql = "INSERT INTO products (name, buying_price, selling_price, display) VALUES ('$name', '$buying_price', '$selling_price', '$display')";
-
-    if(mysqli_query($con, $sql)){
-        echo "Product added successfully.";
-    } else{
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+        if(mysqli_query($con, $sql)){
+            echo "Product added successfully.";
+        } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+        }
+        mysqli_close($con);
+    } else {
+        echo "Name, buying price, and selling price are required fields.";
     }
-    mysqli_close($con);
 }
 ?>
 
@@ -29,14 +39,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Add Product</title>
 </head>
 <body>
-<form method="post" action="addProduct.php" enctype="">
+<form method="post" action="addProduct.php">
     <fieldset style="width: 50%;">
         <legend>ADD PRODUCT</legend>
         Name <br><input type="text" name="name" value=""> <br>
         Buying Price <br><input type="number" name="buying_price" value=""> <br>
         Selling Price <br><input type="number" name="selling_price" value=""> <br>
         <hr>
-        <input type="checkbox" name="display" value=""> Display
+        <input type="checkbox" name="display" value="1"> Display
         <hr>
         <input type="submit" name="save" value="Save">
     </fieldset>
